c83c7e443284e7cb8e221cb3f33c6221f7e05522
Nithal <nithal@qburst.com>
2016-02-10 12:57:36 +0530

    new updates

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,9 @@ var gulp = require('gulp')
 var concat = require('gulp-concat')
 var sass = require('gulp-ruby-sass')
 var jade = require('gulp-jade')
-var prettify = require('gulp-html-prettify')
-var csscomb = require('gulp-csscomb')
+var prettify = require('gulp-jsbeautifier')
 var watch = require('gulp-watch');
 
-gulp.task('default', ['sass', 'jade']);
 
 gulp.task('watch', function () {
    gulp.watch(['build/sass/*.scss', 'build/jade/*.jade'], ['sass', 'jade']);
@@ -16,15 +14,16 @@ gulp.task('sass', function () {
   return sass('build/sass/main.scss')
     .on('error', sass.logError)
     .pipe(concat('style.css'))
-    .pipe(csscomb())
+    .pipe(prettify({indentSize: 2}))
     .pipe(gulp.dest('assets/css/'));
 });
+
 gulp.task('jade', function() {
   var YOUR_LOCALS = {};
   gulp.src('build/jade/*.jade')
     .pipe(jade({
       locals: YOUR_LOCALS
     }))
-    .pipe(prettify({indent_char: ' ', indent_size: 2}))
+    .pipe(prettify({indentSize: 2}))
     .pipe(gulp.dest('assets/'))
 });
